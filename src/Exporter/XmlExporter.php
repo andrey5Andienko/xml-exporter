10c7460849fdc1dd0cbb5efb537203599e5d17be
Andrey Andrienko <andrey_fist@inbox.ru>
2018-07-16 08:46:47 +0300

    Add typehint. Format code

--- src/Exporter/XmlExporter.php
+++ src/Exporter/XmlExporter.php
@@ -6,41 +6,46 @@ use SimpleXMLElement;
 
 class XmlExporter
 {
-    public function jsonToXml($content, string $filename)
+    /** @var string */
+    protected $content;
+
+    public function __construct(string $content)
     {
-        $content = json_decode($content);
+        $this->content = $content;
+    }
+
+    public function save(string $filename): void
+    {
+        $content = json_decode($this->content, true);
+
+        $this->checkVersion($content['version']);
 
         $news = new SimpleXMLElement('<news/>');
-
-        static::checkVersion($content->version);
 
         $this->addChildFromArray($content, $news);
 
         $news->asXML($filename . ".xml");
     }
 
-    public function addChildFromArray($array, SimpleXMLElement &$xml)
+    protected function addChildFromArray(array $array, SimpleXMLElement &$xml): void
     {
         foreach ($array as $key => $value) {
-            if (is_object($value) || is_array($value)) {
+            if (is_array($value)) {
                 $child = $xml->addChild($this->replaceIntToString($key));
                 $this->addChildFromArray($value, $child);
-            } else {
-                $xml->addChild($this->replaceIntToString($key), $value);
+                continue;
             }
+
+            $xml->addChild($this->replaceIntToString($key), $value);
         }
     }
 
-    public function replaceIntToString($value)
+    protected function replaceIntToString($value): string
     {
-        if (is_integer($value)) {
-            return 'item';
-        }
-
-        return $value;
+        return is_integer($value) ? 'item' : $value;
     }
 
-    protected function checkVersion(string $version)
+    protected function checkVersion(string $version): bool
     {
         if ($version === 'https://jsonfeed.org/version/1') {
             return true;
